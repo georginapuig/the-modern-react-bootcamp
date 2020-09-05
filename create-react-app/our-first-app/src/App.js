0368729f82f32d984724a9c944e28eaf0d4bcdce
georgina puig <georginaxcodes@gmail.com>
2020-09-05 17:11:31 -0500

    edit p

--- create-react-app/our-first-app/src/App.js
+++ create-react-app/our-first-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I have edited it
         </p>
         <a
           className="App-link"
