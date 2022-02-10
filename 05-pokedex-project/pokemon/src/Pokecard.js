c887815c10901d6c5348ebb850578652926a9904
georgina <georginaxpuig@gmail.com>
2022-02-10 13:24:35 -0300

    change to fancier imgs

--- 05-pokedex-project/pokemon/src/Pokecard.js
+++ 05-pokedex-project/pokemon/src/Pokecard.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './Pokecard.css';
 
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon';
-
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 class Pokecard extends Component {
   render() { 
-    let imgsrc = `${POKE_API}/${this.props.id}.png`;
+    let imgsrc = `${POKE_API}${padToThree(this.props.id)}.png`;
     
     return (
       <div className='Pokecard'>
