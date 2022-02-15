f28288abe8195b3531f585e3679d6d100e2c9fb0
georgina <georginaxpuig@gmail.com>
2022-02-15 11:39:06 -0300

    add pokegame component

--- 05-pokedex-project/pokemon/src/App.js
+++ 05-pokedex-project/pokemon/src/App.js
@@ -1,10 +1,11 @@
+import React, { Component } from 'react';
 import './App.css';
-import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
 
 function App() {
   return (
     <div className="App">
-      <Pokedex />
+      <Pokegame />
     </div>
   );
 }
--- 05-pokedex-project/pokemon/src/Pokedex.js
+++ 05-pokedex-project/pokemon/src/Pokedex.js
@@ -20,6 +20,8 @@ class Pokedex extends Component {
     return (
       <div className="Pokedex">
         <h1>Pokedex</h1>
+        <p>Total experience: {this.props.exp}</p>
+        <p>{this.props.isWinner ? 'WINNER' : 'LOSER'}</p>
         <div className="Pokedex-cards">
           {this.props.pokemon.map((p) => (
             <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
