261faa9bdd9fff4d9d2817d786196c82a1f223fb
georginapuig <georginaxpuig@gmail.com>
2022-09-29 15:53:55 -0300

    add key value

--- 11-hangman-exercise/react-hangman-starter/src/Hangman.js
+++ 11-hangman-exercise/react-hangman-starter/src/Hangman.js
@@ -46,6 +46,7 @@ class Hangman extends Component {
   generateButtons() {
     return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
       <button
+        key={ltr}
         value={ltr}
         onClick={this.handleGuess}
         disabled={this.state.guessed.has(ltr)}
