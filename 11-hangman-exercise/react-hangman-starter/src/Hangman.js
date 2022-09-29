b258afa11cfed1699d6776e482e490ddaf66e787
georginapuig <georginaxpuig@gmail.com>
2022-09-29 19:31:03 -0300

    add random word

--- 11-hangman-exercise/react-hangman-starter/src/Hangman.js
+++ 11-hangman-exercise/react-hangman-starter/src/Hangman.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { randomWord } from "./words"
 import "./Hangman.css";
 import img0 from "./0.jpg";
 import img1 from "./1.jpg";
@@ -17,23 +18,24 @@ class Hangman extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { nWrong: 0, guessed: new Set(), answer: "apple" };
+    this.state = { nWrong: 0, guessed: new Set(), answer: randomWord() };
     this.handleGuess = this.handleGuess.bind(this);
+    console.log(this.state.answer);
   }
-
+  
   /** guessedWord: show current-state of word:
-    if guessed letters are {a,p,e}, show "app_e" for "apple"
-  */
+   if guessed letters are {a,p,e}, show "app_e" for "apple"
+   */
   guessedWord() {
     return this.state.answer
-      .split("")
-      .map(ltr => (this.state.guessed.has(ltr) ? ltr : "_"));
+    .split("")
+    .map(ltr => (this.state.guessed.has(ltr) ? ltr : "_"));
   }
-
+  
   /** handleGuest: handle a guessed letter:
-    - add to guessed letters
-    - if not in answer, increase number-wrong guesses
-  */
+   - add to guessed letters
+   - if not in answer, increase number-wrong guesses
+   */
   handleGuess(evt) {
     let ltr = evt.target.value;
     this.setState(st => ({
@@ -41,26 +43,26 @@ class Hangman extends Component {
       nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1)
     }));
   }
-
+  
   /** generateButtons: return array of letter buttons to render */
   generateButtons() {
     return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
       <button
-        key={ltr}
-        value={ltr}
-        onClick={this.handleGuess}
-        disabled={this.state.guessed.has(ltr)}
+      key={ltr}
+      value={ltr}
+      onClick={this.handleGuess}
+      disabled={this.state.guessed.has(ltr)}
       >
         {ltr}
       </button>
     ));
   }
-
+  
   /** render: render game */
   render() {
     let endGameLoss = this.state.nWrong >= this.props.maxWrong;
     const altTetx = `${this.state.nWrong} wrong guesses`;
-    
+
     return (
       <div className='Hangman'>
         <h1>Hangman</h1>
