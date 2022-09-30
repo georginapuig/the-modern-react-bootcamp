9c7cddd581ef036e8573e1aa9f229d5fcee3a625
georginapuig <georginaxpuig@gmail.com>
2022-09-30 19:21:25 -0300

    add styling

--- 12-lights-out-game/react-lights-out-starter/src/Board.js
+++ 12-lights-out-game/react-lights-out-starter/src/Board.js
@@ -64,8 +64,6 @@ class Board extends Component {
   /** handle changing a cell: update board & determine if winner */
 
   flipCellsAround(coord) {
-    console.log('flip', coord);
-
     let {ncols, nrows} = this.props;
     let board = this.state.board;
     let [y, x] = coord.split("-").map(Number);
@@ -101,12 +99,12 @@ class Board extends Component {
     // if the game is won, just show a winning msg & render nothing else
 
     // TODO
-    const tblBoard = [];
+    let tblBoard = [];
     
     for (let y = 0; y < this.props.nrows; y++) {
       let row = [];
       for (let x = 0; x < this.props.ncols; x++) {
-        const coord = `${y}-${x}`;
+        let coord = `${y}-${x}`;
         row.push(<Cell key={coord} isLit={this.state.board[y][x]} flipCellsAroundMe={() => this.flipCellsAround(coord)} />)
       }
       tblBoard.push(<tr>{row}</tr>)
@@ -114,13 +112,23 @@ class Board extends Component {
 
     // make table board
     return (
-      <table className='Board'>
-        <tbody>
-          {tblBoard}
-        </tbody>
-      </table>
-    )
-    // TODO
+      <div>
+        {this.state.hasWon ? (
+          <div className='winner'>
+            <span className='neon-orange'>YOU</span>
+            <span className='neon-blue'>WIN!</span>
+          </div>
+        ) : (
+          <div>
+            <div className='Board-title'>
+              <div className='neon-orange'>Lights</div>
+              <div className='neon-blue'>Out</div>
+            </div>
+            {tblBoard}
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
