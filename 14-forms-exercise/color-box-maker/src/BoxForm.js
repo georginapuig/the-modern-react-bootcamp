756d8532fffa8782e42e2d46148b3e08455948d6
georginapuig <georginaxpuig@gmail.com>
2022-10-03 21:16:50 -0300

    add handlechange

--- 14-forms-exercise/color-box-maker/src/BoxForm.js
+++ 14-forms-exercise/color-box-maker/src/BoxForm.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react'
 
 export default class BoxForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      width: '',
+      height: '',
+      color: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    })
+  }
+
   render() {
     return (
-      <div>BoxForm</div>
+      <form>
+        <label htmlFor="width">Width</label>
+        <input type="number" id='width' name='width' value={this.state.width} onChange={this.handleChange} />
+
+        <label htmlFor="height">Height</label>
+        <input type="number" id='height' name='height' value={this.state.height} onChange={this.handleChange} />
+
+        <label htmlFor="color">Color</label>
+        <input type="text" id='color' name='color' value={this.state.color} onChange={this.handleChange} />
+
+        <button>Add box</button>
+      </form>
     )
   }
 }
