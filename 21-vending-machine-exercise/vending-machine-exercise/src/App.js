01ae2e3d4c03e38505de47ab40bf0c5e9319c193
georginapuig <georginaxpuig@gmail.com>
2022-12-30 13:34:22 -0300

    change link to navlink

--- 21-vending-machine-exercise/vending-machine-exercise/src/App.js
+++ 21-vending-machine-exercise/vending-machine-exercise/src/App.js
@@ -4,10 +4,25 @@ import { Routes, Route } from 'react-router-dom';
 import Soda from './Soda';
 import Sardines from './Sardines';
 import Chips from './Chips';
+import { NavLink } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className='App'>
+			<nav>
+				<ul>
+					<li>
+						<NavLink to='/soda'>Soda</NavLink>
+					</li>
+					<li>
+						<NavLink to='/sardines'>Sardines</NavLink>
+					</li>
+					<li>
+						<NavLink to='/chips'>Chips</NavLink>
+					</li>
+				</ul>
+			</nav>
+
 			<Routes>
 				<Route path='/' element={<VendingMachine />} />
 				<Route path='/soda' element={<Soda />} />
