5927e0d62c8170466f2b596802b5c47e4ebb06bc
georgina puig <georginaxpuig@gmail.com>
2023-03-13 17:51:50 -0300

    change form to usecontext

--- 35-using-context-with-hooks/context-demo-app/src/App.js
+++ 35-using-context-with-hooks/context-demo-app/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Navbar from "./Navbar";
 import Form from "./Form";
 import PageContent from "./PageContent";
-import { ThemeProvider, ThemeContext } from "./contexts/ThemeContext";
+import { ThemeProvider } from './contexts/ThemeContext';
 import { LanguageProvider } from "./contexts/LanguageContext";
 
 class App extends Component {
