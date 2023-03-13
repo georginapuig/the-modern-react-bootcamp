fedbf80240bd399f91fc8e34f077f3637217ae10
georgina puig <georginaxpuig@gmail.com>
2023-03-12 22:01:13 -0300

    add context

---
+++ 34-introducing-the-context-api/context-demo-app/src/Form.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import withStyles from "@material-ui/core/styles/withStyles";
+import styles from "./styles/FormStyles";
+import { LanguageContext } from "./contexts/LanguageContext";
+const words = {
+  english: {
+    signIn: "Sign In",
+    email: "Email Address",
+    password: "Password",
+    remember: "Remember Me"
+  },
+  french: {
+    signIn: "Se Connecter",
+    email: "Adresse Électronique",
+    password: "Mot de Passe",
+    remember: "Souviens-toi De Moi"
+  },
+  spanish: {
+    signIn: "Registrarse",
+    email: "Correo Electrónico",
+    password: "Contraseña",
+    remember: "Recuérdame"
+  }
+};
+class Form extends Component {
+  static contextType = LanguageContext;
+  render() {
+    const { language, changeLanguage } = this.context;
+    const { classes } = this.props;
+    const { email, signIn, password, remember } = words[language];
+    return (
+      <main className={classes.main}>
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography variant='h5'>{signIn}</Typography>
+          <Select value={language} onChange={changeLanguage}>
+            <MenuItem value='english'>English</MenuItem>
+            <MenuItem value='french'>French</MenuItem>
+            <MenuItem value='spanish'>Spanish</MenuItem>
+          </Select>
+          <form className={classes.form}>
+            <FormControl margin='normal' required fullWidth>
+              <InputLabel htmlFor='email'>{email}</InputLabel>
+              <Input id='email' name='email' autoFocus />
+            </FormControl>
+            <FormControl margin='normal' required fullWidth>
+              <InputLabel htmlFor='password'>{password}</InputLabel>
+              <Input id='password' name='password' autoFocus />
+            </FormControl>
+            <FormControlLabel
+              control={<Checkbox color='primary' />}
+              label={remember}
+            />
+            <Button
+              variant='contained'
+              type='submit'
+              fullWidth
+              color='primary'
+              className={classes.submit}
+            >
+              {signIn}
+            </Button>
+          </form>
+        </Paper>
+      </main>
+    );
+  }
+}
+export default withStyles(styles)(Form);
---
+++ 34-introducing-the-context-api/context-demo-app/src/Navbar.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
+import Switch from "@material-ui/core/Switch";
+import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles/NavBarStyles";
+import { ThemeContext } from "./contexts/ThemeContext";
+import { withLanguageContext } from "./contexts/LanguageContext";
+
+const content = {
+  english: {
+    search: "Search",
+    flag: "🇬🇧"
+  },
+  french: {
+    search: "Chercher",
+    flag: "🇫🇷"
+  },
+  spanish: {
+    search: "Buscar",
+    flag: "🇪🇸"
+  }
+};
+class Navbar extends Component {
+  static contextType = ThemeContext;
+  render() {
+    const { isDarkMode, toggleTheme } = this.context;
+    const { classes } = this.props;
+    const { language } = this.props.languageContext;
+    const { search, flag } = content[language];
+    return (
+      <div className={classes.root}>
+        <AppBar position='static' color={isDarkMode ? "default" : "primary"}>
+          <Toolbar>
+            <IconButton className={classes.menuButton} color='inherit'>
+              <span>{flag}</span>
+            </IconButton>
+            <Typography className={classes.title} variant='h6' color='inherit'>
+              App Title
+            </Typography>
+            <Switch onChange={toggleTheme} />
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder={`${search}...`}
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput
+                }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+export default withLanguageContext(withStyles(styles)(Navbar));
