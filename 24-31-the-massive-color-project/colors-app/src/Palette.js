69a0fd83397b33ee5370160e6381572f3192139b
georgina puig <georginaxpuig@gmail.com>
2023-03-15 15:55:36 -0300

    add palette

---
+++ 24-31-the-massive-color-project/colors-app/src/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div className='Palette'>
+        {/* navbar */}
+        <div className='Palette-colors'></div>
+        {/* footer */}
+      </div>
+    );
+  }
+}
