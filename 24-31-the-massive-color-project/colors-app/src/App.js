b9bc829d459200a318c3b1e5618cbc2983d77d3a
georgina puig <georginaxpuig@gmail.com>
2023-03-16 01:18:22 -0300

    add color helper

--- 24-31-the-massive-color-project/colors-app/src/App.js
+++ 24-31-the-massive-color-project/colors-app/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { generatePalette } from './ColorsHelpers';
 import seedColors from './seedColors';
 import Palette from './Palette';
 import './App.css';
 class App extends Component {
   render() {
+    console.log(generatePalette(seedColors[4]));
     return (
       <div>
         <Palette {...seedColors[4]} />
