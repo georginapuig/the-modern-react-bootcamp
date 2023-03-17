6b567005130b6f7379039a83c78b9527096321a3
georgina puig <georginaxpuig@gmail.com>
2023-03-17 19:27:00 -0300

    finish mini palette

--- 24-31-the-massive-color-project/colors-app/src/PaletteList.js
+++ 24-31-the-massive-color-project/colors-app/src/PaletteList.js
@@ -6,7 +6,7 @@ import { withStyles } from '@material-ui/styles';
 const styles = {
   root: {
     backgroundColor: 'blue',
-    height: '100%',
+    height: '100vh',
     display: 'flex',
     alignItems: 'flex-start',
     justifyContent: 'center',
@@ -22,7 +22,6 @@ const styles = {
     display: 'flex',
     width: '100%',
     justifyContent: 'space-between',
-    color: 'white',
   },
   palettes: {
     boxSizing: 'border-box',
