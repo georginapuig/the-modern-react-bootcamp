3fa3414c72d80a227404b147e4c049cdfb20d1ae
georgina puig <georginaxpuig@gmail.com>
2023-03-17 12:29:03 -0300

    add palette footer

--- 24-31-the-massive-color-project/colors-app/src/Palette.js
+++ 24-31-the-massive-color-project/colors-app/src/Palette.js
@@ -20,10 +20,10 @@ export default class Palette extends Component {
   }
 
   render() {
-    const { colors } = this.props.palette;
+    const { colors, paletteName, emoji } = this.props.palette;
     const { level, format } = this.state;
     const colorBoxes = colors[level].map((color) => (
-      <ColorBox background={color[format]} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} key={color.id} />
     ));
 
     return (
@@ -34,7 +34,10 @@ export default class Palette extends Component {
           handleChange={this.changeFormat}
         />
         <div className='Palette-colors'>{colorBoxes}</div>
-        {/* footer */}
+        <footer className='Palette-footer'>
+          {paletteName}
+          <span className='emoji'>{emoji}</span>
+        </footer>
       </div>
     );
   }
