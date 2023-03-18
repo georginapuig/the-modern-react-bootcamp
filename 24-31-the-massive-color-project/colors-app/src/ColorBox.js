623a354aa2ffbcf4541b43ff6957faeb57f86b50
georgina puig <georginaxpuig@gmail.com>
2023-03-18 19:41:53 -0300

    adjust text color based on luminance

--- 24-31-the-massive-color-project/colors-app/src/ColorBox.js
+++ 24-31-the-massive-color-project/colors-app/src/ColorBox.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './ColorBox.css';
 import { Link } from 'react-router-dom';
+import chroma from 'chroma-js';
 
 export default class ColorBox extends Component {
   constructor(props) {
@@ -19,6 +20,8 @@ export default class ColorBox extends Component {
   render() {
     const { copied } = this.state;
     const { background, name, moreUrl, showLink } = this.props;
+    const isDarkColor = chroma(background).luminance() <= 0.08;
+    const isLightColor = chroma(background).luminance() >= 0.7;
 
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
@@ -29,17 +32,24 @@ export default class ColorBox extends Component {
           />
           <div className={`copy-msg ${copied && 'show'}`}>
             <h1>copied</h1>
-            <p>{this.props.name}</p>
+            <p className={isLightColor && 'dark-text'}>
+              {this.props.background}
+            </p>
           </div>
+
           <div className='copy-container'>
             <div className='box-content'>
-              <span>{name}</span>
+              <span className={isDarkColor && 'light-text'}>{name}</span>
             </div>
-            <button className='copy-btn'>Copy</button>
+            <button className={`copy-btn ${isLightColor && 'dark-text'}`}>
+              Copy
+            </button>
           </div>
           {showLink && (
             <Link to={moreUrl} onClick={(e) => e.stopPropagation()}>
-              <span className='see-more'>More</span>
+              <span className={`see-more ${isLightColor && 'dark-text'}`}>
+                MORE
+              </span>
             </Link>
           )}
         </div>
--- 24-31-the-massive-color-project/colors-app/src/SingleColorPalette.js
+++ 24-31-the-massive-color-project/colors-app/src/SingleColorPalette.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './Navbar';
 import ColorBox from './ColorBox';
 import PaletteFooter from './PaletteFotter';
+import { Link } from 'react-router-dom';
 
 class SingleColorPalette extends Component {
   constructor(props) {
@@ -31,11 +32,11 @@ class SingleColorPalette extends Component {
 
   render() {
     const { format } = this.state;
-    const { paletteName, emoji } = this.props.palette;
+    const { paletteName, emoji, id } = this.props.palette;
 
     const colorBoxes = this._shades.map((color) => (
       <ColorBox
-        key={color.id}
+        key={color.name}
         name={color.name}
         background={color[format]}
         showLink={false}
@@ -43,9 +44,16 @@ class SingleColorPalette extends Component {
     ));
 
     return (
-      <div className='Palette'>
+      <div className='SingleColorPalette Palette'>
         <Navbar handleChange={this.changeFormat} showingAllColors={false} />
-        <div className='Palette-colors'>{colorBoxes}</div>
+        <div className='Palette-colors'>
+          {colorBoxes}
+          <div className='go-back ColorBox'>
+            <Link to={`/palette/${id}`} className='back-button'>
+              GO BACK
+            </Link>
+          </div>
+        </div>
         <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
