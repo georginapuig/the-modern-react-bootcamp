ab66f7fec1e3f3963795a0e02c7b995722985345
georgina puig <georginaxpuig@gmail.com>
2023-03-19 00:42:52 -0300

    add new palette form

--- 24-31-the-massive-color-project/colors-app/src/App.js
+++ 24-31-the-massive-color-project/colors-app/src/App.js
@@ -5,6 +5,7 @@ import Palette from './Palette';
 import PaletteList from './PaletteList';
 import SingleColorPalette from './SingleColorPalette';
 import seedColors from './seedColors';
+import NewPaletteForm from './NewPaletteForm';
 
 class App extends Component {
   findPalette(id) {
@@ -16,6 +17,7 @@ class App extends Component {
   render() {
     return (
       <Switch>
+        <Route exact path='/palette/new' render={() => <NewPaletteForm />} />
         <Route
           exact
           path='/palette/:paletteId/:colorId'
--- 24-31-the-massive-color-project/colors-app/src/PaletteList.js
+++ 24-31-the-massive-color-project/colors-app/src/PaletteList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import MiniPalette from './MiniPalette';
 import { withStyles } from '@material-ui/styles';
 import styles from './styles/PaletteListStyles';
+import { Link } from 'react-router-dom';
 
 class PaletteList extends Component {
   goToPalette(id) {
@@ -14,6 +15,7 @@ class PaletteList extends Component {
         <div className={classes.container}>
           <nav className={classes.nav}>
             <h1>React Colors</h1>
+            <Link to='/palette/new'>Create Palette</Link>
           </nav>
           <div className={classes.palettes}>
             {palettes.map((palette) => (
