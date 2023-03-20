3f932e6ed21941ad863b7aa1c9d9b46070ff291b
georgina puig <georginaxpuig@gmail.com>
2023-03-19 22:18:21 -0300

    add chrome picker and btns

--- 24-31-the-massive-color-project/colors-app/src/NewPaletteForm.js
+++ 24-31-the-massive-color-project/colors-app/src/NewPaletteForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
+import { ChromePicker } from 'react-color';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
@@ -10,8 +11,9 @@ import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Button from '@material-ui/core/Button';
 
-const drawerWidth = 240;
+const drawerWidth = 400;
 
 const styles = (theme) => ({
   root: {
@@ -125,6 +127,22 @@ class NewPaletteForm extends Component {
             </IconButton>
           </div>
           <Divider />
+          <Typography variant='h4'>Design Your Palette</Typography>
+          <div>
+            <Button variant='contained' color='secondary'>
+              Clear Palette
+            </Button>
+            <Button variant='contained' color='primary'>
+              Random Color
+            </Button>
+          </div>
+          <ChromePicker
+            color='purple'
+            onChangeComplete={(newColor) => console.log(newColor)}
+          />
+          <Button variant='contained' color='primary'>
+            Add Color
+          </Button>
         </Drawer>
         <main
           className={classNames(classes.content, {
