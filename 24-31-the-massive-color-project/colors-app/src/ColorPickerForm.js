0fea583e6c0dacb7259b839f15ca40fa355bb90a
georgina puig <georginaxpuig@gmail.com>
2023-03-23 13:00:49 -0300

    move styles

--- 24-31-the-massive-color-project/colors-app/src/ColorPickerForm.js
+++ 24-31-the-massive-color-project/colors-app/src/ColorPickerForm.js
@@ -3,23 +3,8 @@ import Button from '@material-ui/core/Button';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 import { ChromePicker } from 'react-color';
 import { withStyles } from '@material-ui/core/styles';
+import styles from './styles/ColorPickerFormStyles';
 
-const styles = {
-  picker: {
-    width: '100% !important',
-    marginTop: '2rem',
-  },
-  addColor: {
-    width: '100%',
-    padding: '1rem',
-    marginTop: '1rem',
-    fontSize: '2rem',
-  },
-  colorNameInput: {
-    width: '100%',
-    height: '70px',
-  },
-};
 
 class ColorPickerForm extends Component {
   constructor(props) {
--- 24-31-the-massive-color-project/colors-app/src/PaletteFormNav.js
+++ 24-31-the-massive-color-project/colors-app/src/PaletteFormNav.js
@@ -10,44 +10,8 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
+import styles from './styles/PaletteFormNavStyles';
 
-const drawerWidth = 400;
-const styles = (theme) => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    height: '64px',
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 20,
-  },
-  navBtns: {
-    marginRight: '1rem',
-    '& a': {
-      textDecoration: 'none',
-    },
-  },
-  button: {
-    margin: '0 0.5rem',
-  },
-});
 
 class PaletteFormNav extends Component {
   constructor(props) {
