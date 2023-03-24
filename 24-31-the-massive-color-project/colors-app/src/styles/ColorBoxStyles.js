2cf0b35a0c91f1badc7e50a19762d25ecda7c3ab
georgina puig <georginaxpuig@gmail.com>
2023-03-23 22:37:26 -0300

    make colorbox responsive

--- 24-31-the-massive-color-project/colors-app/src/styles/ColorBoxStyles.js
+++ 24-31-the-massive-color-project/colors-app/src/styles/ColorBoxStyles.js
@@ -1,5 +1,5 @@
 import chroma from 'chroma-js';
-import sizes from '../sizes';
+import sizes from './sizes';
 
 export default {
   ColorBox: {
@@ -15,11 +15,11 @@ export default {
     },
     [sizes.down('lg')]: {
       width: '25%',
-      height: (props) => (props.showingFullPalette ? '20%' : '50%'),
+      height: (props) => (props.showingFullPalette ? '20%' : '33.3333%'),
     },
     [sizes.down('md')]: {
       width: '50%',
-      height: (props) => (props.showingFullPalette ? '10%' : '50%'),
+      height: (props) => (props.showingFullPalette ? '10%' : '20%'),
     },
     [sizes.down('xs')]: {
       width: '100%',
@@ -117,6 +117,9 @@ export default {
       marginBottom: '0',
       padding: '1rem',
       textTransform: 'uppercase',
+      [sizes.down('xs')]: {
+        fontSize: '6rem',
+      },
     },
     '& p': {
       fontSize: '2rem',
