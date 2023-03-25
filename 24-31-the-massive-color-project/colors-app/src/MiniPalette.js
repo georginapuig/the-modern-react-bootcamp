6dad7c292e0effe14197052a339efce9a9c029f1
georgina puig <georginaxpuig@gmail.com>
2023-03-25 05:28:30 -0300

    refactor with PureComponent

--- 24-31-the-massive-color-project/colors-app/src/MiniPalette.js
+++ 24-31-the-massive-color-project/colors-app/src/MiniPalette.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { withStyles } from '@material-ui/styles';
 import styles from './styles/MiniPaletteStyles';
 import DeleteIcon from '@material-ui/icons/Delete';
 
-class MiniPalette extends Component {
+class MiniPalette extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = { DialogShowing: false };
-
     this.deletePalette = this.deletePalette.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
   deletePalette(e) {
     e.stopPropagation();
     this.props.openDialog(this.props.id);
   }
+  handleClick() {
+    this.props.goToPalette(this.props.id);
+  }
   render() {
-    const { classes, paletteName, emoji, colors, handleClick } = this.props;
+    const { classes, paletteName, emoji, colors } = this.props;
+    console.log('RENDERING: ', paletteName);
+
     const miniColorBoxes = colors.map((color) => (
       <div
         className={classes.miniColor}
@@ -24,7 +28,7 @@ class MiniPalette extends Component {
       />
     ));
     return (
-      <div className={classes.root} onClick={handleClick}>
+      <div className={classes.root} onClick={this.handleClick}>
         <DeleteIcon
           className={classes.deleteIcon}
           style={{ transition: 'all 0.3s ease-in-out' }}
