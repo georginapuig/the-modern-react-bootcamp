5f1eebb59dd59df17586d92658b9ed83e028676b
georgina puig <georginaxpuig@gmail.com>
2023-03-27 19:34:20 -0300

    add entrypoint for vendor js file, add bootstrap js

--- 39-webpack/webpack.common.js
+++ 39-webpack/webpack.common.js
@@ -1,7 +1,10 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    main: './src/index.js',
+    vendor: './src/vendor.js',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/template.html',
