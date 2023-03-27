1b10cbdf543e5913838a2ad1bdede5c053bfa336
georgina puig <georginaxpuig@gmail.com>
2023-03-27 15:51:49 -0300

    add prod and dev configs, dev-server

---
+++ 39-webpack/webpack.common.js
@@ -0,0 +1,22 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', //3. Inject styles into DOM
+          'css-loader', //2. Turns css into commonjs
+          'sass-loader', //1. Turns sass into css
+        ],
+      },
+    ],
+  },
+};
