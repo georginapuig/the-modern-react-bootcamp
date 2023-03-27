c7ca0ebc39d42b78570913d544e26c5ffcd6d26f
georgina puig <georginaxpuig@gmail.com>
2023-03-27 20:07:26 -0300

    minify JS, CSS, and HTML in production

--- 39-webpack/webpack.common.js
+++ 39-webpack/webpack.common.js
@@ -1,25 +1,10 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
   entry: {
     main: './src/index.js',
     vendor: './src/vendor.js',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/template.html',
-    }),
-  ],
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader', //3. Inject styles into DOM
-          'css-loader', //2. Turns css into commonjs
-          'sass-loader', //1. Turns sass into css
-        ],
-      },
       {
         test: /\.html$/,
         use: ['html-loader'],
