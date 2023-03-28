20e8a1c69f3bbf5cf8121638e7bd5321162817d3
georgina puig <georginaxpuig@gmail.com>
2023-03-28 18:36:20 -0300

    fetching comments

--- 38-next-fetching-server-api/first-next/.next/static/chunks/webpack.js
+++ 38-next-fetching-server-api/first-next/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fd47b38175b8ef21"; }
+/******/ 		__webpack_require__.h = function() { return "83774f7790d0cbf5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- 38-next-fetching-server-api/first-next/pages/post.js
+++ 38-next-fetching-server-api/first-next/pages/post.js
@@ -1,9 +1,31 @@
-const Post = (props) => <h1>post number {props.id}</h1>;
+import axios from 'axios';
+
+const Post = ({ id, comments }) => {
+  return (
+    <div>
+      <h1>comment for post number {id}</h1>
+      {comments.map((comment) => (
+        <Comment {...comment} key={comment.id} />
+      ))}
+    </div>
+  );
+};
+
+const Comment = ({ email, body }) => (
+  <div>
+    <h5>{email}: </h5>
+    <p>{body}</p>
+  </div>
+);
 
 Post.getInitialProps = async ({ query }) => {
-  // /post?id=${post.id}
-  console.log(query); // { id: "2" }
-  return { id: query.id };
+  console.log(query);
+
+  const res = await axios.get(
+    `https://jsonplaceholder.typicode.com/comments?postId=${query.id}`
+  );
+  const { data } = res;
+  return { ...query, comments: data };
 };
 
 export default Post;
